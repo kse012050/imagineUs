4e898fb2839771bfac51c8d262d812b457347475
김성은 <kse012050@naver.com>
2021-08-24 04:17:10 +0900

    임시 PC 완료 했다

--- js/common.js
+++ js/common.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('header > div > div nav').hover(function(){
         $('header').addClass('active');
-        $('header > div > div nav > ul > li ul').slideDown();
+        $('header > div > div nav > ul > li ul').stop().slideDown();
     },function(){
         $('header').removeClass('active');
-        $('header > div > div nav > ul > li ul').slideUp();
+        $('header > div > div nav > ul > li ul').stop().slideUp();
     })
 });
---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    var scrollArray = [$('[data-scroll = "all"]'),$('[data-scroll = "pc"] > *')];
+    var delta = 0;
+    var offsetTop = 0;
+    $.each(scrollArray , function(){
+        $(this).on('mousewheel',function(e){
+            e.preventDefault();
+            if($('html, body').is(':animated')){
+                return;
+            }
+            delta = e.originalEvent.wheelDelta;
+            
+            if(delta > 0){
+                if($(this).parent().attr('data-scroll') == 'pc' && $(this).index() == 0){
+                    offsetTop = $(this).parent().prev().offset().top;
+                    $('header h1').removeClass('black');
+                }else{
+                    offsetTop = $(this).prev().offset().top;
+                    if($(this).parent().attr('data-scroll') == 'pc' && $(this).index() == 1){
+                        $('header > div > div').removeClass('black');
+                        $('.scrollProgress').removeClass('black');
+                    }
+                }
+            }else{
+                offsetTop = $(this).next().offset().top;
+                if($(this).next().attr('data-scroll') == 'pc'){
+                    $('header h1').addClass('black');
+                }else if($(this).parent().attr('data-scroll') == 'pc'){
+                    $('header > div > div').addClass('black');
+                    $('.scrollProgress').addClass('black');
+                }
+            }
+
+            $('html, body').stop().animate({scrollTop: offsetTop},800); 
+        })
+    })
+
+    $(window).scroll(function(){
+        var scrollProgress = $(window).scrollTop() / ($(document).height() - $(window).height()) * 100;
+        $('.scrollProgress > div > div').css('height',scrollProgress + '%');
+    })
+});
